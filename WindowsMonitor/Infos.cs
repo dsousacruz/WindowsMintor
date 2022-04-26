6b13b13e027942fc7c422a8a77c69544b687585a
Diego de Sousa Cruz <diegocruz@ciandt.com>
2022-04-26 09:54:41 -0300

    Add project

---
+++ WindowsMonitor/Infos.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Management;
+
+namespace WindowsMonitor
+{
+    public class Infos
+    {
+        private PerformanceCounter CpuUsage { get; set; }
+
+        private PerformanceCounter CpuClock { get; set; }
+
+        public Infos()
+        {
+            CpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            CpuClock = new PerformanceCounter("Processor Information", "% Processor Performance", "_Total");
+        }
+
+        public double GetCpuUsage()
+        {
+            return Math.Round(CpuUsage.NextValue(), 2);
+        }
+
+        public double GetCpuClock()
+        {
+            double cpuClockValue = CpuClock.NextValue();
+            double turboSpeed = 0;
+
+            foreach (ManagementObject obj in new ManagementObjectSearcher("SELECT *, Name FROM Win32_Processor").Get())
+            {
+                double maxSpeed = Convert.ToDouble(obj["MaxClockSpeed"]) / 1000;
+                turboSpeed = maxSpeed * cpuClockValue / 100;
+            }
+
+            return Math.Round(turboSpeed * 10, 2);
+
+        }
+
+        public double GetMemory()
+        {
+            var wmiObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+
+            var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new
+            {
+                FreePhysicalMemory = Double.Parse(mo["FreePhysicalMemory"].ToString()),
+                TotalVisibleMemorySize = Double.Parse(mo["TotalVisibleMemorySize"].ToString())
+            }).FirstOrDefault();
+
+            if (memoryValues != null)
+            {
+                return Math.Round(((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize) * 100, 2);
+            }
+
+            return 0;
+        }
+
+        public object GetBattery()
+        {
+            var batteryStatus = "";
+            var designVoltage = "";
+            double estimatedChargeRemaining = 0;
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * FROM Win32_Battery");
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                foreach (PropertyData property in obj.Properties)
+                {
+                    //Console.WriteLine($"Name: {property.Name} | Value: {property.Value}");
+
+                    switch (property.Name)
+                    {
+
+                        case "BatteryStatus":
+                            batteryStatus = GetBaterryStatus(property.Value);
+                            break;
+
+                        case "DesignVoltage":
+                            designVoltage = property.Value.ToString();
+                            break;
+
+                        case "EstimatedChargeRemaining":
+                            estimatedChargeRemaining = Convert.ToDouble(property.Value);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return new
+            {
+                BatteryStatus = batteryStatus,
+                DesignVoltage = designVoltage,
+                EstimatedChargeRemaining = estimatedChargeRemaining
+            };
+        }
+
+        private string GetBaterryStatus(object value)
+        {
+            switch (Convert.ToInt16(value))
+            {
+                case 1:
+                    return "Discharging";
+
+                case 2:
+                    return "AC";
+
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ WindowsMonitor/Program.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+
+namespace WindowsMonitor
+{ 
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            var infos = new Infos();
+            var results = new Dictionary<string, List<dynamic>>();
+            var timeInMinutes = 60;
+            var count = -1;
+
+            while (timeInMinutes > 0)
+            {
+                var metrics = new
+                {
+                    Percentage = infos.GetCpuUsage(),
+                    Clock = infos.GetCpuClock(),
+                    Memory = infos.GetMemory(),
+                    Battery = infos.GetBattery()
+                };
+
+                var key = DateTime.Now.ToString("HH:mm");
+
+                results.TryGetValue(key, out var resultsBySeconds);
+
+                if (resultsBySeconds == null)
+                {
+                    resultsBySeconds = new List<dynamic>();
+                }
+
+                resultsBySeconds.Add(metrics);
+                results[key] = resultsBySeconds;
+
+                count++;
+
+                if (count == 0 || count > 47)
+                {
+                    timeInMinutes--;
+                    count = 0;
+
+                    Console.Write("\rRemaining: {0} minutes", timeInMinutes);
+                }
+
+                //Console.WriteLine(JsonConvert.SerializeObject(metrics));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("End");
+            Console.WriteLine();
+
+            Summary(results);
+        }
+       
+        private static void Summary(Dictionary<string, List<dynamic>> results) 
+        {
+            var summary = new List<object>();
+
+            foreach (var result in results)
+            {
+                var percentagem = 0d;
+                var clock = 0d;
+                var memory = 0d;
+                var battery = 0d;
+
+                foreach (var item in result.Value)
+                {
+                    percentagem += item.Percentage;
+                    clock += item.Clock;
+                    memory += item.Memory;
+                    battery += item.Battery.EstimatedChargeRemaining;
+                }
+
+                percentagem /= result.Value.Count;
+                clock /= result.Value.Count;
+                memory /= result.Value.Count;
+                battery /= result.Value.Count;
+
+                summary.Add(new
+                {
+                    Time = result.Key,
+                    Percentage = Math.Round(percentagem, 2),
+                    Clock = Math.Round(clock, 2),
+                    Memory = Math.Round(memory, 2),
+                    Battery = Math.Round(battery, 2)
+                });
+            }
+
+            Save(summary);
+        }
+
+        private static void Save(object values)
+        {
+            var summaryJson = JsonConvert.SerializeObject(values);
+            var fileName = DateTime.Now.ToString("dd-MM-yy-HH-mm").Trim() + "-log.txt";
+
+            File.WriteAllText(fileName, summaryJson);
+        }
+    }
+}
